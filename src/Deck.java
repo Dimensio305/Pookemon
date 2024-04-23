fef3d0dd8cb5aa7746b5134c04f200ffc7fd5e60
Kilian <ksakhi@unistra.fr>
2024-04-23 17:27:50 +0200

    Création de deck Approximative

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,25 @@
-public class Deck {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Deck {
+    private ArrayList<Pokemon> m_pile= new ArrayList<>();
+
+    /**
+     *
+     */
+    public Deck(int joueur){
+        ArrayList<Integer> doublon= new ArrayList<>();
+        doublon.add(-1);
+        for (int i = 0; this.m_pile.size()<(19+joueur);i++) {
+            int index = new Random().nextInt(Pokemon.getPokedex().size());
+            if (!doublon.contains(index)) {
+                m_pile.add(Pokemon.getPokedex().get(index));
+                index = -1;
+            }
+        }
+    }
+
+    public ArrayList<Pokemon> getPile(){
+        return m_pile;
+    }
 }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,14 +1,63 @@
+import jdk.jshell.Snippet;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Pokemon
 {
-  String m_nom;
-  Type m_type;
-  int m_pv;
-  int m_Attaque;
+  private static ArrayList<Pokemon> pokedex = new ArrayList<>();
+  private static ArrayList<String> nomPokemon = new ArrayList<>(Arrays.asList("Salameche","Reptincel","Dracaufeu", "Carapuce","Carabaffe","Tortank", "Bulbizarre","Herbizarre","Mewtwo","Mew","Artikodin","Elekthor","Sulfura","Magneton","Magnezone","Magneti","Florizarre","Roucool","Roucoups","Roucarnage","Chenipan","Chrysacier","Papilusion","Mimitoss","Aeromite"));
+  private String m_nom;
+  private Type m_type;
+  private int m_pv;
+  private int m_attaque;
 
-public enum Type {FEU,EAU,PLANTE,ACIER,ROCHE,VOL,POISON,GLACE,INSECTE,PSY,TENEBRE,FEE,ELECTRIQUE,NORMAL,COMBAT,SPECTRE,SOL,DRAGON}
+  public enum Type {FEU,EAU,PLANTE,ACIER,ROCHE,VOL,POISON,GLACE,INSECTE,PSY,TENEBRE,FEE,ELECTRIQUE,NORMAL,COMBAT,SPECTRE,SOL,DRAGON}
+
+  public Pokemon(String nom){
+    m_attaque= new Random().nextInt();
+    m_nom=nom;
+    m_pv= new Random().nextInt();
+    m_type= Type.ACIER;
+    pokedex.add(this);
+  }
+
+
+
+
+  /*
+  public boolean estFaible( P okemon other){
+    return faiblesse.Contains(other.m_type);
+  }
+*/
+
+  public static ArrayList<Pokemon> getPokedex(){
+    return pokedex;
+  }
+
   public static void main(String args[])
   {
 
     System.out.println("Lancement du jeu ...");
+    for (String nom: nomPokemon) {
+      new Pokemon(nom);
+    }
+    System.out.println("Choississez le type de votre deck (ou aléatoire pour un deck non-typé):");
+    Deck joueur;
+    Deck IA;
+    if(new Random().nextBoolean()){
+       System.out.println("Vous commencerez la partie !");
+       joueur= new Deck(1);
+       IA= new Deck(2);
+    }else {
+       System.out.println("Vous jouerez en second.");
+       joueur= new Deck(2);
+       IA= new Deck(1);
+    }
+
+    for(Pokemon p : joueur.getPile()) {
+      System.out.println(p.m_nom);
+    }
   }
 }
