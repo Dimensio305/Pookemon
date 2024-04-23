94bd1153c392e8716a6cf98e0c4fff79b54022c2
Kilian <ksakhi@unistra.fr>
2024-04-23 15:30:05 +0200

    Ajout des première classes (Pokemon, Deck, Main et Terrain)

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,7 +1,14 @@
 public class Pokemon
 {
+  String m_nom;
+  Type m_type;
+  int m_pv;
+  int m_Attaque;
+
+public enum Type {FEU,EAU,PLANTE,ACIER,ROCHE,VOL,POISON,GLACE,INSECTE,PSY,TENEBRE,FEE,ELECTRIQUE,NORMAL,COMBAT,SPECTRE,SOL,DRAGON}
   public static void main(String args[])
   {
+
     System.out.println("Lancement du jeu ...");
   }
 }
