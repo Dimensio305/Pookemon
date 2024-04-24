8f2a801d04dfe27d19ad6512cc0388895c22db5d
Liam Guilleux--Riff <guilleuxriff@unistra.fr>
2024-04-24 17:02:02 +0200

    Changement les doublons sont global

--- src/Deck.java
+++ src/Deck.java
@@ -2,17 +2,20 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Deck {
+    private static ArrayList<Integer> doublon= new ArrayList<>();
+
     private ArrayList<Pokemon> m_pile= new ArrayList<>();
+
 
     /**
      *
      */
     public Deck(int joueur){
-        ArrayList<Integer> doublon= new ArrayList<>();
         doublon.add(-1);
         for (int i = 0; this.m_pile.size()<(19+joueur);i++) {
             int index = new Random().nextInt(Pokemon.getPokedex().size());
             if (!doublon.contains(index)) {
+                doublon.add(index);
                 m_pile.add(Pokemon.getPokedex().get(index));
                 index = -1;
             }
