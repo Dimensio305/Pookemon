c2fab68d9b31108e37f922e6cb802d2e7b25c4f9
Liam Guilleux--Riff <guilleuxriff@unistra.fr>
2024-04-24 17:01:10 +0200

    Ajout des pokemon de la première génération

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class Pokemon
 {
   private static ArrayList<Pokemon> pokedex = new ArrayList<>();
-  private static ArrayList<String> nomPokemon = new ArrayList<>(Arrays.asList("Salameche","Reptincel","Dracaufeu", "Carapuce","Carabaffe","Tortank", "Bulbizarre","Herbizarre","Mewtwo","Mew","Artikodin","Elekthor","Sulfura","Magneton","Magnezone","Magneti","Florizarre","Roucool","Roucoups","Roucarnage","Chenipan","Chrysacier","Papilusion","Mimitoss","Aeromite"));
+  private static ArrayList<String> nomPokemon = new ArrayList<>(Arrays.asList("Bulbasaur","Ivysaur","Venusaur","Mega","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Mega","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Mega","Pidgeot","Rattata","Rattata","Raticate","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Raichu","Sandshrew","Sandshrew","Sandslash","Sandslash","NidoranF","Nidorina","Nidoqueen","NidoranM","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Vulpix","Ninetales","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Diglett","Dugtrio","Dugtrio","Meowth","Meowth","Meowth","Persian","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Geodude","Graveler","Graveler","Golem","Golem","Ponyta","Ponyta","Rapidash","Rapidash","Slowpoke","Slowpoke","Slowbro","Slowbro","Magnemite","Magneton","Farfetch'd","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Grimer","Muk","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Mega","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Exeggutor","Cubone","Marowak","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr.Mime","Mr.Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"));
   private String m_nom;
   private Type m_type;
   private int m_pv;
@@ -47,16 +47,20 @@ public class Pokemon
     Deck joueur;
     Deck IA;
     if(new Random().nextBoolean()){
-       System.out.println("Vous commencerez la partie !");
-       joueur= new Deck(1);
-       IA= new Deck(2);
+      System.out.println("Vous commencerez la partie !");
+      joueur= new Deck(1);
+      IA= new Deck(2);
     }else {
-       System.out.println("Vous jouerez en second.");
-       joueur= new Deck(2);
-       IA= new Deck(1);
+      System.out.println("Vous jouerez en second.");
+      joueur= new Deck(2);
+      IA= new Deck(1);
     }
-
+    System.out.println("Deck joueur :");
     for(Pokemon p : joueur.getPile()) {
+      System.out.println(p.m_nom);
+    }
+    System.out.println("\nDeck IA:");
+    for(Pokemon p : IA.getPile()) {
       System.out.println(p.m_nom);
     }
   }
