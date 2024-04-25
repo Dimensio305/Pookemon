668ee05847e6acfc057669dfb4e21b0bab57d8ad
Kilian <ksakhi@unistra.fr>
2024-04-25 12:30:11 +0200

    Ajout d'attribut

--- src/Terrain.java
+++ src/Terrain.java
@@ -1,2 +1,7 @@
+import java.util.ArrayList;
+
 public class Terrain {
+    private ArrayList<Pokemon> m_pokemonJoueur= new ArrayList<>();
+    private ArrayList<Pokemon> m_pokemonIA= new ArrayList<>();
+
 }
