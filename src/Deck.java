129647e8819d1d9e42a5d3bce805664c90c3122b
Kilian <ksakhi@unistra.fr>
2024-04-25 12:30:41 +0200

    Ajout de fonction (estTire et estvide)

--- src/Deck.java
+++ src/Deck.java
@@ -2,27 +2,38 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Deck {
-    private static ArrayList<Integer> doublon= new ArrayList<>();
+    private static ArrayList<Integer> m_doublon= new ArrayList<>();
 
     private ArrayList<Pokemon> m_pile= new ArrayList<>();
+    private ArrayList<Pokemon> m_defausse = new ArrayList<>();
 
 
     /**
      *
      */
     public Deck(int joueur){
-        doublon.add(-1);
+        m_doublon.add(-1);
         for (int i = 0; this.m_pile.size()<(19+joueur);i++) {
             int index = new Random().nextInt(Pokemon.getPokedex().size());
-            if (!doublon.contains(index)) {
-                doublon.add(index);
+            if (!m_doublon.contains(index)) {
+                m_doublon.add(index);
                 m_pile.add(Pokemon.getPokedex().get(index));
                 index = -1;
             }
         }
     }
 
-    public ArrayList<Pokemon> getPile(){
-        return m_pile;
+    public ArrayList<Pokemon> getPile()
+    {
+        return this.m_pile;
+    }
+
+    public void estTire(int index){
+        this.m_pile.remove(index);
+    }
+
+    public boolean estVide()
+    {
+        return this.m_pile.size()>0;
     }
 }
