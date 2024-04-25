87e673f847e1dbc15e533697c2100ac6cbfcc332
Kilian <ksakhi@unistra.fr>
2024-04-25 12:29:27 +0200

    correction de l'enum Type

---
+++ src/Type.java
@@ -0,0 +1,21 @@
+public enum Type {
+    FEU,
+    EAU,
+    PLANTE,
+    ACIER,
+    ROCHE,
+    VOL,
+    POISON,
+    GLACE,
+    INSECTE,
+    PSY,
+    TENEBRE,
+    FEE,
+    ELECTRIQUE,
+    NORMAL,
+    COMBAT,
+    SPECTRE,
+    SOL,
+    DRAGON
+}
+
